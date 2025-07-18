394f17ee6c2456ab117b359c5925b706d8eb3879
nisal <nisalwarnka.1999@gmail.com>
2025-07-18 15:04:43 +0530

    Create incomeExpense model & migration.

---
+++ app/Http/Controllers/income/incomeDetailsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\income;
+
+use App\Http\Controllers\Controller;
+
+class incomeDetailsController extends Controller
+{
+    public function index():string
+    {
+        return 'This is api test with controller index';
+
+    }
+}
---
+++ database/migrations/2025_07_18_092438_create_income_expense_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('income_expense_transactions', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('income_type')->nullable();
+            $table->string('expense_type')->nullable();
+            $table->integer('income_type_id')->nullable();
+            $table->integer('expense_type_id')->nullable();
+            $table->string('special_note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('income_expense_transactions');
+    }
+};
