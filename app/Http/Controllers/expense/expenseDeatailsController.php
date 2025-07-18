f052030caf9a987ee18aa245d87b91819140133b
nisal <nisalwarnka.1999@gmail.com>
2025-07-19 01:56:45 +0530

    Create expense transaction creator, define route,request,controller function.

--- app/Http/Controllers/expense/expenseDeatailsController.php
+++ app/Http/Controllers/expense/expenseDeatailsController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\expense;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ExpenseTransactionCreateRequest;
 use App\Http\Requests\ExpenseTypeCreateRequest;
 use App\Models\ExpenseType;
+use App\Models\IncomeExpenseTransaction;
 use Illuminate\Http\Request;
 
 class expenseDeatailsController extends Controller
@@ -28,5 +30,33 @@ class expenseDeatailsController extends Controller
             return "Expense Type Created Successfully";
         }
 
+    }
+
+    public function ExpenseTransactionCreate(ExpenseTransactionCreateRequest $request):string
+    {
+
+        $AllDataExpenseTransactionCreate = $request->validated();
+
+        $SelectedDataExpenseType = ExpenseType::where('id' , $AllDataExpenseTransactionCreate['ExpenseTypeId']);
+
+        $SelectedExpenseTypeMAxAmount = $SelectedDataExpenseType->get()[0]->max_amount;
+        $SelectedExpenseTypeMinAmount = $SelectedDataExpenseType->get()[0]->min_amount;
+
+        if($SelectedExpenseTypeMAxAmount >= $AllDataExpenseTransactionCreate['ExpenseTransactionAmount'] & $SelectedExpenseTypeMinAmount <= $AllDataExpenseTransactionCreate['ExpenseTransactionAmount'])
+        {
+
+            IncomeExpenseTransaction::create([
+                'expense_type' => $AllDataExpenseTransactionCreate['ExpenseTransactionType'],
+                'expense_type_id' => $AllDataExpenseTransactionCreate['ExpenseTypeId'],
+                'transaction_amount' => $AllDataExpenseTransactionCreate['ExpenseTransactionAmount'],
+                'special_note' => $AllDataExpenseTransactionCreate['ExpenseSpecialNote'],
+
+            ]);
+            return "Expense Transaction Created Successfully";
+        }
+        else
+        {
+            return "Expense Transaction Limitation Error";
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,5 @@ use Illuminate\Support\Facades\Route;
 Route::post('/income-type-create', [IncomeDetailsController::class, 'IncomeTypeCreate']);
 Route::post('/expense-type-create', [expenseDeatailsController::class, 'ExpenseTypeCreate']);
 Route::post('/income-transaction-create', [IncomeDetailsController::class, 'IncomeTransactionCreate']);
+Route::post('/expense-transaction-create', [expenseDeatailsController::class, 'ExpenseTransactionCreate']);
 
